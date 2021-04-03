459631e43f8454c036b9a46560cb5842a08a0392
banksjazz87 <61635862+banksjazz87@users.noreply.github.com>
2021-04-02 22:43:01 -0400

    docs: up to date

--- webpack.config.js
+++ webpack.config.js
@@ -5,12 +5,10 @@ module.exports = {
     entry: './src/client/index.js',
     devtool: 'source-map',
     module: {
-        rules: [
-            {
-                test: '/\.js$/',
-                exclude: /node_modules/,
-                loader: "babel-loader"
-            }
-        ]
+        rules: [{
+            test: '/\.js$/',
+            exclude: /node_modules/,
+            loader: "babel-loader"
+        }]
     }
 }
